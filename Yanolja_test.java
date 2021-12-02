8db2b0bd323d17bcd18d8c21e8a0125e5ba25307
YangHG <40346980+YangHanGil@users.noreply.github.com>
2021-12-02 14:43:10 +0900

    MissingInteger.java

---
+++ Yanolja_test.java
@@ -0,0 +1,50 @@
+package web_test;
+
+import java.util.Arrays;
+
+public class Yanolja_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A[] = {-1, 0, 1, 2, 4};
+		
+		int result = solution(A);
+		System.out.println(result);
+	}
+	
+	public static int solution(int[] A) {
+		
+		int N = 100000;
+		int []arr = new int[N];
+		int cnt=0;
+		
+		int arrKey=0;
+		for (int i : arr) {
+			arr[arrKey] = arrKey+1;
+			arrKey++;
+		}
+		
+		int iKey=0;
+		for (int i : A) {
+			
+			int jKey=0;
+			for (int j : arr) {
+				
+				if(A[iKey]==arr[jKey]) {
+					arr[jKey]=0;
+					cnt++;
+				}
+				
+				jKey++;
+			}
+			
+			iKey++;
+		}
+		
+		Arrays.sort(arr);
+		
+		
+		
+		return arr[cnt];
+    }
+}
