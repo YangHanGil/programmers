d27996366a60c2f85db1080d97da0afd36b17c8d
YangHanGil <hangil968@gmail.com>
2022-01-13 17:45:25 +0900

    코테 3번 푸는중

---
+++ src/goorm_kakaoenterprise2.java
@@ -0,0 +1,53 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class goorm_kakaoenterprise2 {
+
+	//사은품 교환하기
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * 테스트 입력값
+		 * 
+		 
+		4
+		12 0
+		10 14
+		4 20
+		5 2147483648
+		
+		*
+		*
+		*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < testCase; i++) {
+			String str = br.readLine();
+			String[] strArr = str.split(" ");
+			long n = Long.parseLong(strArr[0]);
+			long m = Long.parseLong(strArr[1]);
+			
+			long res = 0;
+			long nSplit = n/5;
+			long nmSplit = (n+m)/12;
+			
+			
+			res = (nSplit<nmSplit) ? nSplit:nmSplit;
+			
+			System.out.println(res);
+		}
+		
+		
+		
+		
+		
+		br.close();
+	}
+	
+}
---
+++ src/goorm_kakaoenterprise3.java
@@ -0,0 +1,48 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class goorm_kakaoenterprise3 {
+
+	//숫자카드게임
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * 테스트 입력값
+		 * 
+		 
+		7 4
+		10 3
+		
+		*
+		*
+		*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String testCase = br.readLine();
+		
+		int n = Integer.parseInt(testCase.split(" ")[0]);
+		int k = Integer.parseInt(testCase.split(" ")[0]);
+		
+		// 0 ~ n-1 까지의 수를 배열에 넣어준다. 
+		int[] intArr = new int[n];
+		int cnt = 0;
+		for (int i : intArr) {
+			intArr[cnt] = cnt;
+			cnt++;
+		}
+		
+		// k갯수의 카드를 몇번 뽑을 수 있는지 판별
+		
+		
+		// 뽑은 k갯수의 카드를 n으로 나눠 0이 되는지 판별
+		
+		
+		
+		br.close();
+	}
+	
+}
